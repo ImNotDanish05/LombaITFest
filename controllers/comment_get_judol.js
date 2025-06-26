6927ea5daaa16822bcc92e0bfa37b801ced6f91e
gyyrroo <syauqialghifari51@gmail.com>
2025-06-26 10:43:25 +0700

    Fix file path for blocked words and add fs dependency in package.json and package-lock.json

--- controllers/comment_get_judol.js
+++ controllers/comment_get_judol.js
@@ -1,4 +1,5 @@
-const LoadData = require('./utils/LoadData');
+const LoadData = require('../utils/LoadData');
+const fs = require('fs');
 const express = require('express');
 
 function getJudolComment(text) {
@@ -6,9 +7,12 @@ function getJudolComment(text) {
     if (text !== normalizedText) { 
         return true
     }
-    const blockedWords = JSON.parse(fs.readFileSync("blockedword.json"));
+    const blockedWords = JSON.parse(fs.readFileSync("./config/blockedword.json"));
 
     const lowerText = text.toLowerCase();
 
     return blockedWords.some(word => lowerText.includes(word.toLowerCase()));
 }
+
+const test = getJudolComment("Arga.");
+console.log(test);
