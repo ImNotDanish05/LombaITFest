e27fa4c003b34499b285d3fe7e188b8d5643747c
gyyrroo <syauqialghifari51@gmail.com>
2025-07-02 11:44:02 +0700

    feat: implement CRUD operations for session management

--- models/Sessions.js
+++ models/Sessions.js
@@ -4,7 +4,7 @@ const sessionSchema = new mongoose.Schema({
     user_google_id: { type: String, required: true }, // ini relasi ke google_id user
     session_id: { type: String, required: true, unique: true }, // ini session token yang nanti disimpan di cookie
     created_at: { type: Date, default: Date.now },
-    expires_at: { type: Date, required: true }
+    expires_at: { type: Date, required: true }
 })
 
 module.exports = mongoose.model('Sessions', sessionSchema);
