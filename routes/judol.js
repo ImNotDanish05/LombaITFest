43fb55fe5e3fc0831a86a9b7bef53069eba8f48e
ImNotDanish05 <danishmahdi2005@gmail.com>
2025-07-16 10:57:31 +0700

    Add flexibility auto AI checklist

--- routes/judol.js
+++ routes/judol.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models/Users');
 const { authSession } = require('../controllers/authSession');
+const isProductionHttps = require('../utils/isProductionHttps');
 
 router.get('/judolremover', authSession, async (req, res) => {
   const userId = req.user;
+  const isHttps = isProductionHttps() ? "1" : "0";
+  console.log(`isProductionHttps: ${isHttps}`);
   if (!userId) return res.redirect('/');
   const user = await Users.findById(userId).lean();
   if (!user) {
@@ -17,7 +20,8 @@ router.get('/judolremover', authSession, async (req, res) => {
       name: user.username,
       email: user.email,
       picture: user.picture
-    }
+    },
+    isProductionHttps: isHttps
   });
 });
 
