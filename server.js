d36fbbe1c58830fa7cc4f18b4ebb93d2e29d4217
ImNotDanish05 <danishmahdi2005@gmail.com>
2025-07-21 19:40:26 +0700

    fix: error javascript

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(async (err, req, res, next) => {
       code: err.status || 500,
       message: err.message || 'Terjadi kesalahan.'
     },
-    backUrl: err.backUrl || '/'
+    backUrl: user ? err.backUrl || '/'
   });
 });
 
